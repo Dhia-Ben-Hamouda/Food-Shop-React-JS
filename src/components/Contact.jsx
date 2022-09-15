9dcdabbf24bec33fa798c67e4bf49ca39332ffec
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2022-09-15 15:47:07 +0100

    working on contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import illustration from "../images/illustration.png";
+import { useState } from "react";
 
 export default function () {
+  const [name, setName] = useState("");
+  const [object, setObject] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
   return (
     <>
       <section id="contact">
@@ -11,7 +17,34 @@ export default function () {
         </div>
         <div className="container">
           <form>
-
+            <div>
+              <input
+                type="text"
+                placeholder="Enter name..."
+                value={name}
+                onChange={(e) => { setName(e.target.value) }}
+              />
+              <input
+                type="text"
+                placeholder="Enter object..."
+                value={object}
+                onChange={(e) => { setObject(e.target.value) }}
+              />
+            </div>
+            <input
+              type="email"
+              placeholder="Enter email..."
+              value={email}
+              onChange={(e) => { setEmail(e.target.value) }}
+            />
+            <textarea
+              type="text"
+              placeholder="Enter message..."
+              value={message}
+              onChange={(e) => { setMessage(e.target.value) }}
+              rows={6}
+            />
+            <button>Send message</button>
           </form>
           <div className="illustration">
             <img src={illustration} />
