717b27afb5331ffe3521d469895495866b84b5a5
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2022-09-16 18:13:12 +0100

    finish about section

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,6 +1,16 @@
 import React, { useRef, useState } from "react";
+import { ImQuotesRight } from "react-icons/im";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination } from "swiper";
+import img1 from "../images/profile1.jpg";
+import img2 from "../images/profile2.jpg";
+import img3 from "../images/profile3.jpg";
+import img4 from "../images/profile4.jpg";
+import img5 from "../images/profile5.jpg";
+import img6 from "../images/profile6.jpg";
+import img7 from "../images/profile7.jpg";
+import img8 from "../images/profile8.jpg";
+import img9 from "../images/profile9.jpg";
 import "swiper/css/pagination";
 import "swiper/css";
 
@@ -22,49 +32,118 @@ export default function App() {
       >
         <SwiperSlide className="slide">
           <div className="top">
-
+            <ImQuotesRight className="icon"/>
+          </div>
+          <div className="middle">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis temporibus, maiores cupiditate et repudiandae iure esse reiciendis numquam deserunt </p>
+          </div>
+          <div className="bottom">
+            <img src={img1} />
+            <h3>Customer name</h3>
           </div>
         </SwiperSlide>
         <SwiperSlide className="slide">
           <div className="top">
-
+            <ImQuotesRight className="icon"/>
+          </div>
+          <div className="middle">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis temporibus, maiores cupiditate et repudiandae iure esse reiciendis numquam deserunt </p>
+          </div>
+          <div className="bottom">
+            <img src={img2} />
+            <h3>Customer name</h3>
           </div>
         </SwiperSlide>
         <SwiperSlide className="slide">
           <div className="top">
+            <ImQuotesRight className="icon"/>
+          </div>
+          <div className="middle">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis temporibus, maiores cupiditate et repudiandae iure esse reiciendis numquam deserunt </p>
+          </div>
+          <div className="bottom">
+            <img src={img3} />
+            <h3>Customer name</h3>
+          </div>
+        </SwiperSlide>
 
+        <SwiperSlide className="slide">
+          <div className="top">
+            <ImQuotesRight className="icon"/>
+          </div>
+          <div className="middle">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis temporibus, maiores cupiditate et repudiandae iure esse reiciendis numquam deserunt </p>
+          </div>
+          <div className="bottom">
+            <img src={img4} />
+            <h3>Customer name</h3>
           </div>
         </SwiperSlide>
         <SwiperSlide className="slide">
           <div className="top">
-
+            <ImQuotesRight className="icon"/>
+          </div>
+          <div className="middle">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis temporibus, maiores cupiditate et repudiandae iure esse reiciendis numquam deserunt </p>
+          </div>
+          <div className="bottom">
+            <img src={img5} />
+            <h3>Customer name</h3>
           </div>
         </SwiperSlide>
         <SwiperSlide className="slide">
           <div className="top">
+            <ImQuotesRight className="icon"/>
+          </div>
+          <div className="middle">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis temporibus, maiores cupiditate et repudiandae iure esse reiciendis numquam deserunt </p>
+          </div>
+          <div className="bottom">
+            <img src={img6} />
+            <h3>Customer name</h3>
+          </div>
+        </SwiperSlide>
 
+
+        <SwiperSlide className="slide">
+          <div className="top">
+            <ImQuotesRight className="icon"/>
+          </div>
+          <div className="middle">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis temporibus, maiores cupiditate et repudiandae iure esse reiciendis numquam deserunt </p>
+          </div>
+          <div className="bottom">
+            <img src={img7} />
+            <h3>Customer name</h3>
           </div>
         </SwiperSlide>
         <SwiperSlide className="slide">
           <div className="top">
-
+            <ImQuotesRight className="icon"/>
+          </div>
+          <div className="middle">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis temporibus, maiores cupiditate et repudiandae iure esse reiciendis numquam deserunt </p>
+          </div>
+          <div className="bottom">
+            <img src={img8} />
+            <h3>Customer name</h3>
           </div>
         </SwiperSlide>
         <SwiperSlide className="slide">
           <div className="top">
-
+            <ImQuotesRight className="icon"/>
+          </div>
+          <div className="middle">
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perferendis temporibus, maiores cupiditate et repudiandae iure esse reiciendis numquam deserunt </p>
+          </div>
+          <div className="bottom">
+            <img src={img9} />
+            <h3>Customer name</h3>
           </div>
         </SwiperSlide>
-        <SwiperSlide className="slide">
-          <div className="top">
 
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="slide">
-          <div className="top">
-
-          </div>
-        </SwiperSlide>
+        
+        
         
       </Swiper>
     </>
