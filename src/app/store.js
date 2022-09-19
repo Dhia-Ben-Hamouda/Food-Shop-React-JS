c8123a8f23c49caec70ed518fdb5c08472d7ea85
Dhia Ben Hamouda <dhiabenhamouda.5@gmail.com>
2022-09-19 11:53:54 +0100

    working on responsiveness

--- src/app/store.js
+++ src/app/store.js
@@ -2,6 +2,21 @@ import { legacy_createStore as createStore } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import rootReducer from "./rootReducer.js";
 
-const store = createStore(rootReducer , composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
+
+// function solution() 
+// {
+//   for (i = 0; i <= n; ++i) 
+//   {
+//     if (!( (i+1) % 4)) 
+//     { 
+//       delay = 400;
+//     }
+//     else
+//     {
+//       delay = ( (i+1) % 4) * 100;
+//     } 
+//   }
+// }
